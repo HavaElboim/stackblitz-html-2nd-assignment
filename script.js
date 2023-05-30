8d65800bca50ccf7ea4774bed6b63abe71f5796d
HavaElboim <77002425+HavaElboim@users.noreply.github.com>
2023-05-30 22:19:33 +0300

    3 files modified

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+let move_click = document.getElementById('onClickText');
+let tmp, tmp2, tmp3;
+let jump_x = 100;
+
+let screen_width = window.screen.width;
+let max_width = Math.min(700, screen_width);
+console.log('screen width is ', screen_width, ' max width is ', max_width);
+
 function moveOnClick() {
-  // TODO :: Add logic here
+  console.log(move_click.style.left);
+
+  // .style.left is the position of the element from the left of the screen
+  // .replace performs a regex. Here it replaces all non-integer characters (\D) with an empty character
+  tmp = Number(move_click.style.left.replace(/\D/g, ''));
+  console.log(tmp);
+
+  // add the increment set at the beginning of the script
+  tmp2 = tmp + jump_x;
+  console.log(tmp2);
+
+  //set the new position from the left
+  if (tmp2 < max_width) {
+    move_click.style.left = tmp2 + 'px';
+    console.log('adding jump');
+  } else {
+    move_click.style.left = '0px';
+    console.log('resetting to 0');
+  }
 }
