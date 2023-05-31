e717e945c9a843046e7032916919b24c0b9bfd5d
HavaElboim <77002425+HavaElboim@users.noreply.github.com>
2023-05-31 17:54:27 +0300

    3 files modified

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Text is provided in the HTML file, Make it move on click
 You can change it to move on hover / mouse enter / mouse leave
 
 Also, you can create more then one text that preform in the different types.
-Must use classes here!
+
+****\*\***** Must use classes here! ****\*\*\*\*****
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let move_click = document.getElementById('onClickText');
+console.log('move_click is: ', move_click);
 let move_hover = document.getElementById('onHoverText');
-let tmp, tmp2, tmp3;
+let tmp, tmp_left, tmp_top, tmp2, tmp2_left, tmp2_top, tmp3;
 let jump_x = 100,
   jump_y = 50;
 
@@ -9,6 +10,50 @@ let screen_height = window.screen.height;
 let max_width = Math.min(700, screen_width);
 let max_height = Math.min(400, screen_height);
 console.log('screen width is ', screen_width, ' max width is ', max_width);
+
+class movingBox {
+  constructor(element_to_move, jump_top, jump_left) {
+    this.jump_top = jump_top;
+    this.jump_left = jump_left;
+    this.element_to_move = element_to_move;
+  }
+  //method:
+  moveOnAction() {
+    // .style.left is the position of the element from the left of the screen
+    console.log(this.element_to_move.style.left);
+
+    // .replace performs a regex. Here it replaces all non-integer characters (\D) with an empty character
+    tmp_left = Number(this.element_to_move.style.left.replace(/\D/g, ''));
+    tmp_top = Number(this.element_to_move.style.top.replace(/\D/g, ''));
+    console.log(tmp_left, tmp_top);
+
+    // add the increment given in the class constructor
+    tmp2_left = tmp_left + this.jump_left;
+    tmp2_top = tmp_top + this.jump_top;
+    console.log(tmp2_left, tmp2_top);
+
+    //set the new position from the left
+    if (tmp2_left < max_width) {
+      this.element_to_move.style.left = tmp2_left + 'px';
+      console.log('adding jump from left');
+    } else {
+      this.element_to_move.style.left = '0px';
+      console.log('resetting left to 0');
+    }
+
+    //set the new position from the top
+    if (tmp2_top < max_height) {
+      this.element_to_move.style.top = tmp2_top + 'px';
+      console.log('adding jump from top');
+    } else {
+      this.element_to_move.style.top = '0px';
+      console.log('resetting top to 0');
+    }
+  }
+}
+
+let clickBox = new movingBox(move_click, 100, 0);
+let hoverBox = new movingBox(move_hover, 0, 50);
 
 function moveOnClick() {
   console.log(move_click.style.left);
